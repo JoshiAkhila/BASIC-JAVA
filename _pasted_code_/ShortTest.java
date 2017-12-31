425b62954991f1596ff49c22cdc8668025bbb45e
JoshiAkhila <joshi.akhila@gmail.com>
2017-12-31 14:34:43 -0800

    initial commit

---
+++ _pasted_code_/ShortTest.java
@@ -0,0 +1,22 @@
+package _pasted_code_;
+
+public class ShortTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short s1 = 32; 
+		// 365,35000,65398. will through error, because of different data type.
+	    System.out.println("A short: " +  s1);
+	    int s2 = 365;
+	    System.out.println("A int: " +  s2);
+	    long s3 = 12345678901234567l;
+	    System.out.println("A long: " +  s3);
+	    float s4 = 65398.0f;
+	    System.out.println("A float: " +  s4);
+	    double s5 = 1212673984d;
+	    System.out.println("A double: " +  s5);
+	    char s6 = 'A';
+	    System.out.println("A char: " +  s6);
+	}
+
+}
