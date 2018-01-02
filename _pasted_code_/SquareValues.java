89cc11da520d5b8b6d8da4e0b9123befac6e6d4d
JoshiAkhila <joshi.akhila@gmail.com>
2018-01-01 23:29:55 -0800

    loop test

---
+++ _pasted_code_/SquareValues.java
@@ -0,0 +1,14 @@
+package _pasted_code_;
+
+public class SquareValues {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 10;
+		for (int i = 1; i <= n; i++) {
+			int square = i * i;
+			System.out.println(i + " " + square);
+		}
+
+	}
+}
