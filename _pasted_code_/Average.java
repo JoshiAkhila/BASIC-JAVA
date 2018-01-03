830fc050e15cd361dc5e114a5068f9d669a858f6
JoshiAkhila <joshi.akhila@gmail.com>
2018-01-02 16:00:23 -0800

    Arithematic operations

---
+++ _pasted_code_/Average.java
@@ -0,0 +1,19 @@
+package _pasted_code_;
+
+public class Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sub1=45;
+		int sub2=50;
+		int sub3=47;
+		int sub4=49;
+		int sub5=48;
+		int sub6=40;
+		int total = sub1+sub2+sub3+sub4+sub5+sub6;
+		int Average = total/6;
+		System.out.println("Total = "+ total );
+		System.out.println("Average = "+ Average );
+	}
+
+}
---
+++ _pasted_code_/Averageloop.java
@@ -0,0 +1,21 @@
+package _pasted_code_;
+
+public class Averageloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sub1 = 45;
+		int sub2 = 50;
+		int sub3 = 47;
+		int sub4 = 49;
+		int sub5 = 48;
+		int sub6 = 40;
+		for (int i = 0; i < 3; i++) {
+			int total = sub1 + sub2 + sub3 + sub4 + sub5 + sub6;
+			int Average = total / 6;
+			System.out.println("Total = " + total);
+			System.out.println("Average = " + Average);
+		}
+	}
+
+}
