49f5f57134c1b01c2383e97dc401160d66936464
JoshiAkhila <joshi.akhila@gmail.com>
2018-01-02 16:28:47 -0800

    if-else for discounte

---
+++ _pasted_code_/Discount.java
@@ -0,0 +1,19 @@
+package _pasted_code_;
+
+import java.util.Scanner;
+
+public class Discount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter the ItemPrice");
+		double ItemPrice = scan.nextDouble();
+
+		double DiscountPrice = ItemPrice - (ItemPrice * 5) / 100; // 5% discount
+		System.out.println("Discount(5%) price for an Item is: " + DiscountPrice);
+
+	}
+
+}
