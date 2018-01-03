a9c6eebb93471baeea0ee1e63618b0357129b5cc
JoshiAkhila <joshi.akhila@gmail.com>
2018-01-02 19:45:22 -0800

    initial commit

---
+++ _pasted_code_/Index_Array_Element.java
@@ -0,0 +1,24 @@
+package _pasted_code_;
+
+public class Index_Array_Element {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = new int[10];
+		arr[0]=1; 
+		arr[1]=2;
+		arr[2]=4;
+		arr[3]=8;
+		arr[4]=16;
+		arr[5]=32;
+		arr[6]=16;
+		arr[7]=8;
+		arr[8]=4;
+		arr[9]=2;
+		for(int i=0;i<10;i++){
+			System.out.println(i+" "+arr[i]);
+		}
+		
+	}
+
+}
